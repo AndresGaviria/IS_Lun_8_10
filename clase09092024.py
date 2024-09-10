79ad94f6e979bb848214f808033b0314d7859488
AndresGaviria <Andres.Gaviria@sleepgroupsolutions.com>
2024-09-09 19:00:02 -0500

    Clase complate del 09-09-2024

--- clase09092024.py
+++ clase09092024.py
@@ -15,6 +15,21 @@ print("Hola mundo");
   varBool: bool = True; # bool(True)
 """
 
+class Estados:
+    id: int;
+
+    def GetId(self) -> int: 
+        return self.id;
+    def SetId(self, valor: int) -> None: 
+        self.id = valor;
+
+    nombre: str;
+
+    def GetNombre(self) -> str: 
+        return self.nombre;
+    def SetNombre(self, valor: str) -> None: 
+        self.nombre = valor;
+
 class Conexion:
     string_conexion: str = """
         Driver={MySQL ODBC 9.0 Unicode Driver};
@@ -36,5 +51,68 @@ class Conexion:
 
         conexion.close();
 
+    def ConnectionPersonas(self) -> None:  
+        conexion = pyodbc.connect(self.string_conexion);
+        
+        consulta: str = """
+            SELECT p.id,
+                p.cedula,
+                p.nombre,
+                p.estado,
+                p.fecha,
+                p.activo,
+                e.id,
+                e.nombre
+            FROM personas p 
+                INNER JOIN estados e ON p.estado = e.id""";
+        cursor = conexion.cursor();
+        cursor.execute(consulta);
+
+        for elemento in cursor:
+            print(elemento);
+
+        conexion.close();
+
+    def ConnectionBasicaTipo(self) -> None:  
+        conexion = pyodbc.connect(self.string_conexion);
+        
+        consulta: str = """SELECT * FROM estados""";
+        cursor = conexion.cursor();
+        cursor.execute(consulta);
+
+        lista: list = [];
+        for elemento in cursor:
+            estado: Estados = Estados();
+            estado.SetId(elemento[0]);
+            estado.SetNombre(elemento[1]);
+            lista.append(estado);
+
+        conexion.close();
+
+        for estado in lista:
+            print(str(estado.GetId()) + " - " + estado.GetNombre());
+
+    
+    def ConnectionNonQuery(self) -> None:  
+        conexion = pyodbc.connect(self.string_conexion);
+        cursor = conexion.cursor();
+        
+        nombre: str = "Python estado";
+
+        consulta: str = "INSERT INTO `estados` (`nombre`) VALUES ('" + nombre + "')";
+        cursor.execute(consulta);
+        cursor.execute("SELECT LAST_INSERT_ID()");
+        cursor.commit();  
+
+        print("Filas afectadas: " + str(cursor.fetchone()[0]));
+        cursor.close();
+        conexion.close();
+
 conexion: Conexion = Conexion();
-conexion.ConnectionBasica();
+# conexion.ConnectionBasica();
+conexion.ConnectionNonQuery();
+conexion.ConnectionBasicaTipo();
+
+estado: Estados = Estados();
+estado.SetId(0);
+estado.SetNombre("Test");
---
+++ script.sql
@@ -0,0 +1,33 @@
+CREATE USER 'user_ptyhon'@'localhost' IDENTIFIED BY 'Clas3s1Nt2024_!';
+GRANT CREATE, INSERT, UPDATE, DELETE, SELECT, FILE, EXECUTE ON *.* TO 'user_ptyhon'@'localhost' WITH GRANT OPTION;
+
+CREATE DATABASE db_personas;
+
+CREATE TABLE `db_personas`.`estados` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `nombre` VARCHAR(50) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `db_personas`.`personas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cedula` VARCHAR(50) NOT NULL ,
+  `nombre` VARCHAR(50) NOT NULL,
+  `estado` INT NOT NULL,
+  `fecha` DATETIME NOT NULL,
+  `activo` BIT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_personas__estados` FOREIGN KEY (`estado`) REFERENCES `estados` (`id`)
+);
+
+INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Casado-Casada');
+INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Soltero-Soltera');
+INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Viudo-Viuda');
+INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Separado-Separada');
+INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Lo que importa es que Diosito me quiere');
+
+INSERT INTO `db_personas`.`personas` (`cedula`, `nombre`, `estado`, `fecha`, `activo`) 
+VALUES ('154287565', 'Pepito Perez', 1, NOW(), 1);
+
+INSERT INTO `db_personas`.`personas` (`cedula`, `nombre`, `estado`, `fecha`, `activo`) 
+VALUES ('789546541', 'Susana Martinez', 2, NOW(), 0);
