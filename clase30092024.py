3c41292f8bd0bcef1ec169933eeb82ce2d032cbc
AndresGaviria <Andres.Gaviria@sleepgroupsolutions.com>
2024-10-07 09:54:22 -0500

    Test 07-10-2024

--- clase30092024.py
+++ clase30092024.py
@@ -1,11 +1,10 @@
 import sys;
 import jsonify;
 import flask;
-import sys;
 import json;
 import datetime;
 
-print("API 1 en Python con VS Code");
+print("Clase del 30-09-2024");
 print(__name__);
 app = flask.Flask(__name__);
 
@@ -21,6 +20,10 @@ class Convertir:
             print(sys.exc_info());
             return None;
 
+diccionario = Convertir.ADict("{'Id':'1', 'Cedula':'76782687'}");
+print(diccionario["Id"]);
+print(diccionario["Cedula"]);
+
 # http://localhost:4040/main2/ObtenerDatos/{"Send":"Test"}
 @app.route('/main2/ObtenerDatos/<string:entrada>', methods=["GET"]) # methods=["POST"]
 def ObtenerDatos(entrada: str) -> str :
@@ -35,13 +38,3 @@ def ObtenerDatos(entrada: str) -> str :
         return flask.jsonify(respuesta);
 
 app.run('localhost', 4040);
-
-"""
-py -3 --version
-py main2.py
-py -m pip install pyodbc
-py -m pip install Flask
-py -m pip install jsonify
-
-py -m pip uninstall jsonify
-""" 
