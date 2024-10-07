efc87d64fb6f8b22c7599a395922a1e6a58dbe28
AndresGaviria <Andres.Gaviria@sleepgroupsolutions.com>
2024-10-07 18:20:48 -0500

    Trabajo en clase 07-10-2024

---
+++ clase07102024.py
@@ -0,0 +1,23 @@
+""" Trabajo para 07-10-2024
+    Crear un WEB SERIVE (API) Consultando
+    una base de datos, que se asigno en el 
+    trabajo anterior, Para ser realizado en 
+    clase para nota de seguimiento
+
+Temas
+1. Habitaciones		MENDOZA GOEZ CRISTIAN ANDRES
+2. Piscinas		ARIAS GIRALDO DIEGO MAURICIO
+3. Animales		FLOREZ CABADIAS CIELO INES
+4. Estudiantes		GARCES CARDENAS OSVALDO
+5. Capitales		CHAVES MONTOYA JUAN FELIPE
+6. Planetas		ZAPATA VALENCIA SEBASTIAN
+7. Productos		ALZATE LOAIZA SEBASTIAN
+8. Balones		ECHAVARRIA SANCHEZ DUBAN ESTID
+9. Bailes		ATENCIA CARO CRISTIAN JOSE
+10. Deportes		FLOREZ VALENCIA JOHN ANDREI
+11. Ecosistemas		MOSQUERA PEREA JOSYMAR
+12. Hospitales		PATIÑO JHONY EDUARDO
+13. Zapatos		USMA RESTREPO JOHAN ESTIVEN
+14. Vehiculos		LONDOÑO PEREZ JULIAN STIVEN
+
+"""
